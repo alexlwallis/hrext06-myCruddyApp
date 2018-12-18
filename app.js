6419796d9255b4cc2d3d145ecb69a8c36e747509
Alex Wallis <aldwallis@Alexs-MacBook-Air.local>
2018-12-17 21:56:19 -0800

    added ability to store numerous values and delete them. Also when deleting a key the display updates to show the prior key

--- app.js
+++ app.js
@@ -2,23 +2,37 @@ $(document).ready(function(){
   console.log('jQuery loaded');
 
   // write to local storage from input when button save clicked
+  var value = 0
+
   $('.btn-submit').on('click', function(){
+    value++
+    localStorage.setItem(value, $('.text-entry').val());
+    var myItemInStorage = localStorage.getItem(value);
 
-    //set limit of 250 things that can be in localStorage before??
-
-
-    localStorage.setItem('inputFieldValue', $('.text-entry').val());
-    var myItemInStorage = localStorage.getItem('inputFieldValue');
-    console.log('myItemInStorage', myItemInStorage);
-
-    // display the value here
-    $('.list-display-field').text(myItemInStorage); // ??
+    // display the value here, displays what we put in the box
+    //TODO: Need to have display change when 
+    $('.list-display-field').text($('.text-entry').val()); // ??
 
   });
 
   // delete from local storage when delete button clicked
+  // using the length property of localStorage I find out how many pairs there are
+  // and using the 
   $('.btn-delete').on('click', function(){
-    localStorage.removeItem('inputFieldValue');
+    var len = localStorage.length
+    var key = localStorage.key(len-1)
+    localStorage.removeItem(len);
+
+
+    // Now when item is deleted from localStorage the list-display-field updates
+    // to show the prior item
+
+    if (len > 0){
+      $('.list-display-field').text(key);
+    } else {
+      $('.list-display-field').text('')
+    }
+
   });
 
   // delete all from local storage when clicked
